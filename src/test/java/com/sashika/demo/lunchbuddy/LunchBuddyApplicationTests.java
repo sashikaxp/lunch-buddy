1c9a7b680e2946896f72a40ee5c4de87c547c6df
SashikaPeiris <sashikaxp@gmail.com>
2023-08-19 08:43:30 +0530

    Initial commit

---
+++ src/test/java/com/sashika/demo/lunchbuddy/LunchBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sashika.demo.lunchbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
